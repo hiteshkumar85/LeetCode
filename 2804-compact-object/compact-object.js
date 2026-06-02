c79a517a770a246189e95ce65b04a852e79fae3c
Hitesh Kumar <hiteshkumar853391@gmail.com>
2026-06-02 23:20:04 +0530

    Time: 87 ms (13.37%) | Memory: 70.2 MB (9.99%) - LeetSync

---
+++ 2804-compact-object/compact-object.js
@@ -0,0 +1,30 @@
+/**
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+var compactObject = function (obj) {
+
+    if (Array.isArray(obj)) {
+        return obj
+            .map(compactObject)
+            .filter(Boolean);
+    }
+
+    if (obj !== null && typeof obj === "object") {
+
+        const result = {};
+
+        for (const [key, value] of Object.entries(obj)) {
+
+            const compacted = compactObject(value);
+
+            if (Boolean(compacted)) {
+                result[key] = compacted;
+            }
+        }
+
+        return result;
+    }
+
+    return obj;
+};
