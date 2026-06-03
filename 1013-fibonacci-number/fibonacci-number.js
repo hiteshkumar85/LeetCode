39e1e9c159642cf3a93e86b459dddab4a1e9ade1
Hitesh Kumar <hiteshkumar853391@gmail.com>
2026-06-03 23:54:27 +0530

    Time: 37 ms (91.90%) | Memory: 53.7 MB (47.05%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -3,10 +3,16 @@
  * @return {number}
  */
 var fib = function (n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
+    if (n === 0 || n === 1) {
+        return n;
     }
-    return fib(n - 1) + fib(n - 2);
+    let a = 0;
+    let b = 1;
+    let result = 0;
+    for (i = 2; i <= n; i++) {
+        result = a + b;
+        a = b;
+        b = result;
+    }
+    return result;
 };
